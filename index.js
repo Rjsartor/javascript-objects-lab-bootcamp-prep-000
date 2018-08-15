343c3a5e9094bad4aa948c6f21a7e905eed07526
randal.sartor@gmail.com <randal.sartor@gmail.com>
2018-08-15 21:42:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ function updateObjectWithKeyAndValue(recipes, name, ingredients) {
   return Object.assign( {}, recipes, {[name]: ingredients})}
 
 function destructivelyUpdateObjectWithKeyAndValue(recipes, name, ingredients) {
-  recipes[name] 
+  recipes[name] = ingredients
+  return recipes
 }
